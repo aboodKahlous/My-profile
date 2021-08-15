23d7a95c66f0e3e1dd6719971ab721176dbf6efb
aboodKahlous <86069394+aboodKahlous@users.noreply.github.com>
2021-08-15 08:28:27 +0300

    heigth

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+// function aboutme(){
+//     document.getElementById("center").style.transform = "translateY(0)";
+//     document.getElementById("right").style.transform = "translateY(0)";
+//     document.getElementById("skill").style.transform = "translateY(0)"
+// }
+
+// function skill(){
+//     document.getElementById("center").style.transform = "translateY(-120%)";
+//     document.getElementById("right").style.transform = "translateY(-120%)";
+//     document.getElementById("skill").style.transform = "translateY(-100%)";
+// }
+
+// $("nav li").on('click' , function(){
+//     $(this).addClass('active').siblings().removeClass('active');
+// })
