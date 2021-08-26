e38db90216ade1f08d2943a5061ed7811cac5897
aboodKahlous <86069394+aboodKahlous@users.noreply.github.com>
2021-08-26 21:24:21 +0300

    finsh

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,29 @@
 $("nav li").on('click' , function(){
     $(this).addClass('active').siblings().removeClass('active');
 })
+
+
+///////////////////// button to top 
+var scrollButtom = $("#buttom-up");
+$(window).scroll(function() {
+
+    if ($(this).scrollTop() >= 300) {
+        scrollButtom.fadeIn(1000);
+    } else if ($(this).scrollTop() <= 300) {
+        scrollButtom.fadeOut(1000);
+    }
+})
+scrollButtom.click(function() {
+    $("html,body").animate({
+         scrollTop: 0     
+    }, 1000);
+})
+
+
+$(document).ready(function(){
+
+    $("header i").click(function(){
+        $(this).toggleClass("fa-align-left fa-times");
+    })
+
+});
