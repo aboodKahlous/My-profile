62a2c4c8f2782db60e29786a61c315ace3c04aeb
aboodKahlous <86069394+aboodKahlous@users.noreply.github.com>
2021-08-26 19:20:45 +0300

    edit

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,3 @@
-// function aboutme(){
-//     document.getElementById("center").style.transform = "translateY(0)";
-//     document.getElementById("right").style.transform = "translateY(0)";
-//     document.getElementById("skill").style.transform = "translateY(0)"
-// }
-
-// function skill(){
-//     document.getElementById("center").style.transform = "translateY(-120%)";
-//     document.getElementById("right").style.transform = "translateY(-120%)";
-//     document.getElementById("skill").style.transform = "translateY(-100%)";
-// }
-
-// $("nav li").on('click' , function(){
-//     $(this).addClass('active').siblings().removeClass('active');
-// })
+$("nav li").on('click' , function(){
+    $(this).addClass('active').siblings().removeClass('active');
+})
